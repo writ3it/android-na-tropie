ac4fd7c404581d1d06ac781014fe1bed3131539a
writ3it <42292507+writ3it@users.noreply.github.com>
2019-05-12 18:44:18 +0200

    wyświetlanie posta

--- app/src/main/java/pl/zhp/natropie/ReaderActivity.kt
+++ app/src/main/java/pl/zhp/natropie/ReaderActivity.kt
@@ -2,10 +2,16 @@ package pl.zhp.natropie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.app_bar_reader.*
+import kotlinx.android.synthetic.main.content_reader.*
+import org.parceler.Parcels
+import pl.zhp.natropie.db.entities.PostWithColor
+import pl.zhp.natropie.db.types.DateConverter
+import pl.zhp.natropie.ui.NaTropiePage
 
 class ReaderActivity : AppCompatActivity() {
 
@@ -13,6 +19,17 @@ class ReaderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reader)
         setSupportActionBar(toolbar)
+        initView();
+    }
+
+    private fun initView() {
+        val post =intent.getParcelableExtra<Parcelable>(VAR_POST).let { Parcels.unwrap<PostWithColor>(it) }
+        val doc = NaTropiePage(post.content)
+        doc.setTitle(post.title)
+            .setAuthor(post.author)
+            .setDate(post.date)
+            .setCategory(post.category)
+        web_content.loadDataWithBaseURL("",doc.getHtml(),"text/html","UTF-8","")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
